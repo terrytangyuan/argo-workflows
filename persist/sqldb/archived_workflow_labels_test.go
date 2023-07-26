19aa79b1cbefb03eac14f5bd15f4a96cc0595d75
Yuan Tang <terrytangyuan@gmail.com>
2023-07-26 11:02:57 -0400

    wip
    Signed-off-by: Yuan Tang <terrytangyuan@gmail.com>

--- persist/sqldb/archived_workflow_labels_test.go
+++ persist/sqldb/archived_workflow_labels_test.go
@@ -2,26 +2,26 @@ package sqldb
 
 import (
 	"cloud.google.com/go/cloudsqlconn"
-	"database/sql"
-	"fmt"
+	"context"
 	"github.com/jackc/pgx/v4"
 	"github.com/jackc/pgx/v4/stdlib"
 	"github.com/stretchr/testify/assert"
+	postgresqladp "github.com/upper/db/v4/adapter/postgresql"
+	"k8s.io/apimachinery/pkg/labels"
 	"net"
 	"testing"
-
-	"context"
-	"k8s.io/apimachinery/pkg/labels"
+	"upper.io/db.v3/postgresql"
 )
 
 func Test_labelsClause(t *testing.T) {
+	assert.Equal(t, 1, 1)
 	// Ref: https://cloud.google.com/sql/docs/postgres/iam-logins#log-in-with-automatic
 	d, err := cloudsqlconn.NewDialer(context.Background(), cloudsqlconn.WithIAMAuthN())
 	assert.Nil(t, err)
 	var opts []cloudsqlconn.DialOption
 
-	dsn := fmt.Sprintf("user=%s database=%s", "p774999980047-mame9g@gcp-sa-cloud-sql.iam.gserviceaccount.com", "postgres")
-	instanceConnectionName := "akuity-test:us-west2:akp-tenantdb001-15-tst-usw2-primary"
+	dsn := "user=akp-tndb001-15-tst-usw2-app@akuity-test.iam database=testdb sslmode=disable"
+	instanceConnectionName := "akuity-test:us-west2:akp-tenantdb001-15-tst-usw2"
 	config, err := pgx.ParseConfig(dsn)
 	assert.Nil(t, err)
 
@@ -31,15 +31,20 @@ func Test_labelsClause(t *testing.T) {
 	println(config.ConnString())
 	dbURI := stdlib.RegisterConnConfig(config)
 	// "missing \"=\" after \"registeredConnConfig0\" in connection info string\"
-	//connURL, err := postgresql.ParseURL(dbURI)
-	//assert.Nil(t, err)
-	//options := map[string]string{
-	//	"sslmode": "disable",
-	//}
-	//connURL.Options = options
-	//_, err = postgresql.Open(connURL)
-	db, err := sql.Open("pgx", dbURI)
+	connURL, err := postgresqladp.ParseURL(dbURI)
 	assert.Nil(t, err)
+	options := map[string]string{
+		"sslmode": "disable",
+	}
+	connURL.Options = options
+	db, err := postgresqladp.Open(
+		postgresql.ConnectionURL{
+			Database: `testdb`,
+			User:     `akp-tndb001-15-tst-usw2-app@akuity-test.iam`,
+			Options:  options,
+		})
+	//db, err := sql.Open("pgx", dbURI)
+	//assert.Nil(t, err)
 	// failed SASL auth
 	err = db.Ping()
 	assert.Nil(t, err)
---
+++ test_db.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"cloud.google.com/go/cloudsqlconn"
+	"context"
+	"database/sql"
+	"github.com/jackc/pgx/v4"
+	"github.com/jackc/pgx/v4/stdlib"
+	"log"
+	"net"
+)
+
+func main() {
+	// Ref: https://cloud.google.com/sql/docs/postgres/iam-logins#log-in-with-automatic
+	d, err := cloudsqlconn.NewDialer(context.Background(), cloudsqlconn.WithIAMAuthN())
+	if err != nil {
+		log.Fatal("NewDialer: ", err)
+	}
+	var opts []cloudsqlconn.DialOption
+	dsn := "user=akp-tndb001-15-tst-usw2-app@akuity-test.iam database=testdb sslmode=disable"
+	instanceConnectionName := "akuity-test:us-west2:akp-tenantdb001-15-tst-usw2"
+	config, err := pgx.ParseConfig(dsn)
+	if err != nil {
+		log.Fatal("ParseConfig: ", err)
+	}
+	config.DialFunc = func(ctx context.Context, network, instance string) (net.Conn, error) {
+		return d.Dial(ctx, instanceConnectionName, opts...)
+	}
+	dbURI := stdlib.RegisterConnConfig(config)
+	sess, err := sql.Open("pgx", dbURI)
+	if err != nil {
+		log.Fatal("sql.Open: ", err)
+	}
+	defer sess.Close()
+	err = sess.Ping()
+	if err != nil {
+		log.Fatal("ping: ", err)
+	}
+}
